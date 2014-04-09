64ed7e99fdd849ed51c82930c28f172c8b0eec2e
Lucaszw <lucas.zeer@gmail.com>
2014-04-08 22:02:57 -0400

    fixing navbar

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140222020008) do
+ActiveRecord::Schema.define(version: 20140313033241) do
 
   create_table "catagories", force: true do |t|
     t.datetime "created_at"
@@ -156,6 +156,7 @@ ActiveRecord::Schema.define(version: 20140222020008) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "sketchfab_token"
+    t.string   "email"
   end
 
 end
